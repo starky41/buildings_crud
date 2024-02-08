35d35acbbb03b18f15ed1191f693219c0e1cf6c0
starky <maxim333rrr@gmail.com>
2024-02-09 00:45:56 +1200

    initial commit

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import psycopg2
+from db_executable import sql_create_tables
 
 # This connection string should match the one in your Python code
 conn_string = "host='localhost' dbname='my_db' user='starky' password='1234'"
@@ -9,60 +10,7 @@ conn = psycopg2.connect(conn_string)
 # Create a cursor object
 cursor = conn.cursor()
 
-cursor.execute(
-
-    """
-    CREATE TABLE IF NOT EXISTS street(
-        ID_street INT PRIMARY KEY,
-        street_name VARCHAR(150)
-    );
-    
-    CREATE TABLE IF NOT EXISTS type_construction(
-        ID_type_construction INT PRIMARY KEY,
-        type_construction_name VARCHAR(150)
-    );
-    
-    CREATE TABLE IF NOT EXISTS basic_project(
-        ID_basic_project INT PRIMARY KEY,
-        basic_project_name VARCHAR(150)
-    );
-    
-    CREATE TABLE IF NOT EXISTS appointment(
-        ID_basic_project INT PRIMARY KEY,
-        appointment_name VARCHAR(150)
-    );
-    
-    CREATE TABLE IF NOT EXISTS load_bearing_walls(
-        ID_load_bearing_walls INT PRIMARY KEY,
-        load_bearing_walls_name VARCHAR(150)
-    );
-    
-    CREATE TABLE IF NOT EXISTS building_roof(
-        ID_building_roof INT PRIMARY KEY,
-        building_floor_name VARCHAR(150)
-    );
-    
-    CREATE TABLE IF NOT EXISTS building_floor(
-        ID_building_floor INT PRIMARY KEY,
-        building_floor_name VARCHAR(150)
-    );
-    
-    CREATE TABLE IF NOT EXISTS facade(
-        ID_facade INT PRIMARY KEY,
-        facade_name VARCHAR(150)
-    );
-    
-    CREATE TABLE IF NOT EXISTS foundation(
-        ID_foundation INT PRIMARY KEY,
-        foundation_name VARCHAR(150)    
-    );
-    
-    CREATE TABLE IF NOT EXISTS management_company(
-        ID_management_company INT PRIMARY KEY,
-        management_company_name VARCHAR(150)
-    );
-    
-""")
+cursor.execute(sql_create_tables)
 
 conn.commit()
 
--- db_executable.py
+++ db_executable.py
@@ -1,16 +1,52 @@
-
-
-
-
-
-"""
-
-    CREATE TABLE IF NOT EXISTS street
-    (
-        ID_street PRIMARY KEY,
-        street_name VARCHAR(150),
-
-    )
-
-
+sql_create_tables = """
+    CREATE TABLE IF NOT EXISTS street(
+        ID_street INT PRIMARY KEY,
+        street_name VARCHAR(150)
+    );
+    
+    CREATE TABLE IF NOT EXISTS type_construction(
+        ID_type_construction INT PRIMARY KEY,
+        type_construction_name VARCHAR(150)
+    );
+    
+    CREATE TABLE IF NOT EXISTS basic_project(
+        ID_basic_project INT PRIMARY KEY,
+        basic_project_name VARCHAR(150)
+    );
+    
+    CREATE TABLE IF NOT EXISTS appointment(
+        ID_basic_project INT PRIMARY KEY,
+        appointment_name VARCHAR(150)
+    );
+    
+    CREATE TABLE IF NOT EXISTS load_bearing_walls(
+        ID_load_bearing_walls INT PRIMARY KEY,
+        load_bearing_walls_name VARCHAR(150)
+    );
+    
+    CREATE TABLE IF NOT EXISTS building_roof(
+        ID_building_roof INT PRIMARY KEY,
+        building_floor_name VARCHAR(150)
+    );
+    
+    CREATE TABLE IF NOT EXISTS building_floor(
+        ID_building_floor INT PRIMARY KEY,
+        building_floor_name VARCHAR(150)
+    );
+    
+    CREATE TABLE IF NOT EXISTS facade(
+        ID_facade INT PRIMARY KEY,
+        facade_name VARCHAR(150)
+    );
+    
+    CREATE TABLE IF NOT EXISTS foundation(
+        ID_foundation INT PRIMARY KEY,
+        foundation_name VARCHAR(150)    
+    );
+    
+    CREATE TABLE IF NOT EXISTS management_company(
+        ID_management_company INT PRIMARY KEY,
+        management_company_name VARCHAR(150)
+    );
+    
 """
