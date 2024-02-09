1658f0e87063c6d2a9eabc48a089101a619d84dd
starky <maxim333rrr@gmail.com>
2024-02-10 00:49:56 +1200

    created tables

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 import psycopg2
-from db_executable import sql_create_tables
+from db_executable import sql_query
 
 # This connection string should match the one in your Python code
 conn_string = "host='localhost' dbname='my_db' user='starky' password='1234'"
@@ -10,7 +10,15 @@ conn = psycopg2.connect(conn_string)
 # Create a cursor object
 cursor = conn.cursor()
 
-cursor.execute(sql_create_tables)
+
+cursor.execute(
+    '''DROP TABLE IF EXISTS street, type_construction, basic_project, appointment, load_bearing_walls, building_roof, 
+    building_floor, facade, foundation, management_company, building_description CASCADE;
+    
+    '''
+
+)
+cursor.execute(sql_query)
 
 conn.commit()
 
--- db_executable.py
+++ db_executable.py
@@ -1,3 +1,8 @@
+# A helper function to generate foreign key statements.
+def create_foreign_key(fk_field, reference_table):
+    return f", FOREIGN KEY ({fk_field}) REFERENCES {reference_table}({fk_field})"
+
+
 sql_create_tables = """
     CREATE TABLE IF NOT EXISTS street(
         ID_street INT PRIMARY KEY,
@@ -15,7 +20,7 @@ sql_create_tables = """
     );
     
     CREATE TABLE IF NOT EXISTS appointment(
-        ID_basic_project INT PRIMARY KEY,
+        ID_appointment INT PRIMARY KEY,
         appointment_name VARCHAR(150)
     );
     
@@ -50,3 +55,59 @@ sql_create_tables = """
     );
     
 """
+
+sql_create_building_description = f'''
+    CREATE TABLE IF NOT EXISTS building_description(
+        ID_building INT PRIMARY KEY,
+        ID_street INT,
+        house INT,
+        building_body INT,
+        latitude NUMERIC,
+        longitude NUMERIC,
+        year_construction INT,
+        number_floors INT,
+        number_entrances INT,
+        number_buildings INT,
+        number_living_quarters INT,
+        title VARCHAR(150),
+        ID_type_construction INT,
+        ID_basic_project INT,
+        ID_appointment INT,
+        seismic_resistance_max NUMERIC,
+        seismic_resistance_min NUMERIC,
+        seismic_resistance_soft NUMERIC,
+        zone_SMZ_min NUMERIC,
+        zone_SMZ_max NUMERIC,
+        zone_SMZ_increment NUMERIC,
+        priming VARCHAR(150),
+        ID_load_bearing_walls INT,
+        basement_area NUMERIC,
+        ID_building_roof INT,
+        ID_building_floor INT,
+        ID_facade INT,
+        ID_foundation INT,
+        azimuth VARCHAR(150),
+        cadastral_number INT,
+        cadastral_cost NUMERIC,
+        year_overhaul INT,
+        accident_rate VARCHAR(150),
+        ID_management_company INT,
+        Land_area NUMERIC,
+        notes VARCHAR(150),
+        author VARCHAR(150)
+        
+        {create_foreign_key('ID_street', 'street')}
+        {create_foreign_key('ID_type_construction', 'type_construction')}
+        {create_foreign_key('ID_basic_project', 'basic_project')}
+        {create_foreign_key('ID_appointment', 'appointment')}
+        {create_foreign_key('ID_load_bearing_walls', 'load_bearing_walls')}
+        {create_foreign_key('ID_building_roof', 'building_roof')}
+        {create_foreign_key('ID_building_floor', 'building_floor')}
+        {create_foreign_key('ID_facade', 'facade')}
+        {create_foreign_key('ID_foundation', 'foundation')}  
+        {create_foreign_key('ID_management_company', 'management_company')}     
+    
+    );
+'''
+
+sql_query = sql_create_tables + sql_create_building_description
