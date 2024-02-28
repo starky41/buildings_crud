6c001da3070d9c928c3ef644642d164312689247
Maksim Balakin <maxim333rrr@gmail.com>
2024-02-28 18:56:00 +1200

    removed unneeded code from main

--- main.py
+++ main.py
@@ -2,33 +2,13 @@ from database import init_db, db_session
 from data_access_layer import DataAccessLayer
 from gui import MyApp
 from PyQt6.QtWidgets import QApplication
-# Initialize the database (create tables if they don't exist)
+
 init_db()
 
-# Instantiate the data access layer with the database session
 dal = DataAccessLayer(db_session)
 
 def main():
-    from models import Appointment
-    
-    # Usage:
-    dal = DataAccessLayer(db_session)
-    
-
-    # Create operation example
-    # new_appointment = Appointment(appointment_name='test_appointment1')
-    # dal.create(new_appointment)
-
-    # Read operation example
-    # appointments = dal.read(Appointment)
-    # specific_appointment = dal.read(Appointment, ID_appointment=2)
-
-    # # Update operation example
-    # dal.update(Appointment, {'ID_building': 1}, house=11)
-
-    # # Delete operation example
-    # dal.delete(Appointment, ID_building=1)
-
+    print('main')
 if __name__ == "__main__":
     
     main()
