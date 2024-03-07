7eba138c9ff4ad89e7a96d16134acbf3130306fb
Maksim Balakin <maxim333rrr@gmail.com>
2024-03-08 01:44:05 +1200

    made recreation and initial population of the database more intuitive

--- database/database.py
+++ database/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 from credentials import username, password, database, host, port
+from sqlalchemy.exc import IntegrityError
 
 
 Base = declarative_base()
@@ -14,18 +15,17 @@ Base.query = db_session.query_property()
 
 def init_db():
     from database.populate_database import populate_database
-    # Import all of the models
-    # This will associate the models with the Base metadata
+
     
-    # import models
-    # Base.metadata.create_all(bind=engine)
-
     # for testing purposes only (delete tables and records)
-    Base.metadata.drop_all(engine)
-
+    # Base.metadata.drop_all(engine)
 
     # Create all tables in the engine. This is equivalent to "Create Table" statements in raw SQL.
     Base.metadata.create_all(engine)
-    populate_database()
-  
+    try:
+        populate_database()
+    except IntegrityError as e:
+        print("THE VALUES YOU ARE TRYING TO ADD WITH populate_database() already exist in the table")
+        pass
+
     
